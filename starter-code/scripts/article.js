0311b8999086c4b73fac27737b53b8552fa73315
crashtack <crashtack@gmail.com>
2016-06-07 12:16:12 -0700

    after inclass edits

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,10 +3,18 @@ var articles = [];
 function Article (opts) {
   // TODO: Use the object passed in to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
-
+  this.title = opts.title;
+  this.category = opts.category;
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 // Define a method on the Article prototype
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-catagory', this.cateory);
 
   /* TODO: Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
@@ -18,18 +26,27 @@ function Article (opts) {
    5. publication date. */
 
   // Display the date as a relative number of "days ago":
-
-
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   /* TODO: This cloned article is no longer a template,
    as it now has real data attached to it! We need to account
    for that and change it before this current article gets
    rendered to our DOM. */
-
+  return $newArticle;
+};
 
 // Sort our data by date published, descending order
-
+ourLocalData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
 /* Now iterate through our transformed collection and instantiate
  a new Article instance for each object in our collection. */
+ourLocalData.forEach(function(ele){
+  articles.push(new Article(ele));
+});
 
 /* Append each Article to the DOM.
 NOTE: Remember that the '.toHtml' method invoked is one WE created. */
+articles.forEach(function(article){
+  $('#articles').append(article.toHtml());
+});
